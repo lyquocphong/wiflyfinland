186c431c67d726564d56fca2b0d0564aae665f71
Ly Quoc Phong <lyquocphong@gmail.com>
2019-11-13 19:43:34 +0200

    Init repo

---
+++ www/application/views/templates/default.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ 
+    <head>
+        <title><?php echo $title; ?></title>
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+ 
+    <body>
+              
+        <div class="container mx-auto h-screen">
+             
+            <?php echo $body; ?>
+             
+        </div>
+         
+    </body>
+     
+</html>
